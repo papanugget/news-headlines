cc776f5e71bd5ef96ee50714d9607df68889ba5e
papanugget <evilgenius04@gmail.com>
2019-04-18 17:19:53 -0400

    added semantic ui via npm

--- headlines-v3/src/components/App.js
+++ headlines-v3/src/components/App.js
@@ -1,14 +1,58 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import news from '../api/news';
 
 class App extends React.Component {
+    // state to contain news sources and selected source
+    state = {
+        language: [],
+        sources: [],
+        selectedLanguage: '',
+        selectedSource: ''
+    };
+    // get sources list from axios / news api onload
+    componentDidMount = async () =>  {
+        const res = await news.get(`sources?${news.defaults.headers.Authorization}`);
+        if(res.status === 200) {
+            this.setState({sources: res.data.sources});
+        }
+        const languages = this.state.sources.map ( source => {
+            // console.log(source.language)
+            return source.language
+        }).filter((lang, idx, arr) => {
+            return idx === arr.indexOf(lang);
+        })
+        this.setState({language: languages});
+    }
+    
     render() {
+        // console.log(this.state);
         return (
-            <div>
+            <div className="ui container">
                 This is the App component
-                <div>
-                    <SearchBar/>
+                <div className="ui two column stackable grid">
+                    <div className="column">
+                        <div className="ui segment">
+                            <div className="ui fluid search selection dropdown">
+                            <i className="dropdown icon"></i>
+                                <div className="default text">Select a language</div>
+                                <SearchBar language={this.state.language}/>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div className="ui segment">
+                            <div className="ui fluid search selection dropdown">
+                            <i className="dropdown icon"></i>
+                                <div className="default text">Select a news source</div>
+                                <SearchBar sources={this.state.sources}/>
+                            </div>
+                            
+                        </div>
+                    </div>
                 </div>
+                
             </div>
         )
     }
--- headlines-v3/src/components/SearchBar.js
+++ headlines-v3/src/components/SearchBar.js
@@ -1,10 +1,32 @@
 import React from 'react';
 
-class SearchBar extends React.Component {
-    render() {
-        return (
-            <div>This is the search bar</div>
-        )
+const SearchBar = (props) => {
+    console.log(props);
+    // if props.item is language return a language selection option
+    if(props.language) {
+        const items = props.language.map( (language, idx) => {
+            return (
+                <div 
+                    className="item" 
+                    key={idx} 
+                    data-value={language}
+                >{language}
+                </div>
+            )
+        });
+        return <div className="menu">{items}</div>
+    } else if(props.sources) {
+        const items = props.sources.map( (source, idx) => {
+            return (
+                    <div 
+                        className="item" 
+                        key={idx} 
+                        data-value={source.url}
+                    >{source.name}
+                    </div>
+            )
+        });
+        return <div className="menu">{items}</div>
     }
 }
 
